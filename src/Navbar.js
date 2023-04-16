e7afbdd91d1fdc4fa9c0493fc2a2acfe0643ed19
Josue Belacy <jbelacy@gmail.com>
2023-04-16 17:39:18 -0400

    created some components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,13 @@
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <h1> Reaction to React.js</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
