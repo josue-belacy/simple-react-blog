9b5976fa6881d99d47e56a9c0abb0d26bd4e5088
Josue Belacy <jbelacy@gmail.com>
2023-04-16 20:32:24 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-
   return (
     <div className="App">
       <Navbar />
-      <div className="content"></div>
-      <Home />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
