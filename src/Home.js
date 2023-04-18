50eb82087acbe6f8c38efa36d32c6553803a8f16
Josue Belacy <jbelacy@gmail.com>
2023-04-17 20:18:04 -0400

    ouputting lists using useState

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new blog", body: "lorem ipsum...", author: "Jackie", id: 1 },
+    {
+      title: "How did React become popular",
+      body: "lorem ipsum...",
+      author: "Melinda",
+      id: 2,
+    },
+    {
+      title: "Web development tools ",
+      body: "lorem ipsum...",
+      author: "Anne",
+      id: 3,
+    },
+  ]);
   return (
-    <div className="Home">
-      <h2>Homepage</h2>
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
